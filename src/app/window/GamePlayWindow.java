b9221057fda57b8e823708507e6bf18c03bb8ed0
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-01-11 23:37:53 -0700

    Updated Form for regions
    temporary fill color applied to check sizes.

--- src/app/window/GamePlayWindow.java
+++ src/app/window/GamePlayWindow.java
@@ -5,17 +5,22 @@ import java.awt.*;
 
 public class GamePlayWindow {
     private JPanel OuterJPanel;
+    private JPanel JPanelShapesNext;
+    private JPanel JPanelCanvasMain;
+    private JPanel JPanelScores;
+    private JPanel JPanelShapes;
+    private JPanel JPanelNextShape;
+    private JTextArea JTextAreaScoresList;
     private JButton ButtonForSizeTest;
 
     public GamePlayWindow() {
-        // generic lambda action listener calls this print statement for demonstrating functionality
-        ButtonForSizeTest.addActionListener(e -> System.out.println("Test Button Pressed!"));
+
     }
 
     public void createAndShowGUI() {
         // near minimum code to generate and show a one button application of arbitrary size
-        JFrame frame = new JFrame("Title Goes Here");
-        frame.setMinimumSize(new Dimension(800, 600));
+        JFrame frame = new JFrame("TETROS BUBBLES");
+        frame.setMinimumSize(new Dimension(660, 960));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(OuterJPanel);
 
