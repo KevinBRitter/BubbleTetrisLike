ee37394a0adfcb01a8db30b5e16a7d8e8bd7b0d5
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-01-12 21:31:27 -0700

    slight refactoring
    thinking through adding multiple images to a panel.  no clear insight at this time.

--- src/app/window/GamePlayWindow.java
+++ src/app/window/GamePlayWindow.java
@@ -4,25 +4,29 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePlayWindow {
-    private JPanel OuterJPanel;
+    private JPanel JPanelGamePlay;
     private JPanel JPanelShapesNext;
     private JPanel JPanelCanvasMain;
     private JPanel JPanelScores;
     private JPanel JPanelShapes;
     private JPanel JPanelNextShape;
     private JTextArea JTextAreaScoresList;
-    private JButton ButtonForSizeTest;
 
     public GamePlayWindow() {
-
+        ImageIcon icon = null;
+        Image image;
+        icon = new ImageIcon(getClass().getResource("/images/raw_blue.png"));
+        image = icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        ImageIcon box = new ImageIcon(image);
+//        JPanelCanvasMain.add(box);
     }
 
     public void createAndShowGUI() {
-        // near minimum code to generate and show a one button application of arbitrary size
+        // Displays the game window
         JFrame frame = new JFrame("TETROS BUBBLES");
         frame.setMinimumSize(new Dimension(660, 960));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(OuterJPanel);
+        frame.add(JPanelGamePlay);
 
         frame.pack();
         frame.setVisible(true);
